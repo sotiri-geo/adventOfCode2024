2d9b6634ad8174ffac2787ee59bba2d72576931e
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-08-15 17:15:16 +0100

    feat: tested example for part1 solution

--- internal/day02/solve.go
+++ internal/day02/solve.go
@@ -1,5 +1,10 @@
 package day02
 
+import (
+	"strconv"
+	"strings"
+)
+
 const (
 	lowerBound = 1
 	upperBound = 3
@@ -24,4 +29,47 @@ func IsIncreasing(report []int) bool {
 
 func DecreaseWithinBounds(firstValue, secondValue int) bool {
 	return IncreaseWithinBounds(secondValue, firstValue)
+}
+
+func IsDecreasing(report []int) bool {
+	var decreasing = true
+
+	for i := 1; i < len(report); i++ {
+		decreasing = decreasing && DecreaseWithinBounds(report[i-1], report[i])
+	}
+
+	return decreasing
+}
+
+func IsSafe(report []int) bool {
+	return IsIncreasing(report) || IsDecreasing(report)
+}
+
+func toInts(strs []string) []int {
+	// good to use make(type, n) when you know the array is of fixed size n
+	// no extra allocation of memory as we append to array
+	nums := make([]int, len(strs))
+
+	for i, s := range strs {
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
+		nums[i] = n
+	}
+	return nums
+}
+
+// Part 1 solution
+func Part1(inputs []string) int {
+	var safeCount = 0
+
+	for _, report := range inputs {
+		parsedReport := toInts(strings.Fields(report))
+		if IsSafe(parsedReport) {
+			safeCount++
+		}
+	}
+
+	return safeCount
 }
--- internal/day02/solve_test.go
+++ internal/day02/solve_test.go
@@ -73,3 +73,69 @@ func TestIsIncreasing(t *testing.T) {
 		}
 	})
 }
+
+func TestIsDecreasing(t *testing.T) {
+	t.Run("report is decreasing", func(t *testing.T) {
+		given := []int{7, 6, 4, 2, 1}
+		got := IsDecreasing(given)
+		want := true
+
+		if got != want {
+			t.Errorf("got %v, want %v, given %v", got, want, given)
+		}
+	})
+
+	t.Run("report is not decreasing", func(t *testing.T) {
+		given := []int{7, 6, 8, 2, 1}
+		got := IsDecreasing(given)
+		want := false
+
+		if got != want {
+			t.Errorf("got %v, want %v, given %v", got, want, given)
+		}
+	})
+}
+
+func TestIsSafe(t *testing.T) {
+	safeReportTest := map[string]struct {
+		Report []int
+		Want   bool
+	}{
+		"Increasing report is safe":                         {Report: []int{1, 3, 5, 6, 7}, Want: true},
+		"Decreasing report is safe":                         {Report: []int{7, 6, 4, 2, 1}, Want: true},
+		"Increase of 5 between 2 and 7 makes report unsafe": {Report: []int{1, 2, 7, 8, 9}, Want: false},
+		"Decrease of 4 between 6 and 2 makes report unsafe": {Report: []int{9, 7, 6, 2, 1}, Want: false},
+	}
+
+	for name, tt := range safeReportTest {
+		t.Run(name, func(t *testing.T) {
+			got := IsSafe(tt.Report)
+
+			if got != tt.Want {
+				t.Errorf("got %v, want %v from report %v", got, tt.Want, tt.Report)
+			}
+		})
+	}
+}
+
+func TestPart1(t *testing.T) {
+	t.Run("One report is safe", func(t *testing.T) {
+		inputs := []string{"7 6 4 2 1"}
+		got := Part1(inputs)
+		want := 1
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+
+	t.Run("Two out of six reports are safe", func(t *testing.T) {
+		inputs := []string{"7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9"}
+		got := Part1(inputs)
+		want := 2
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+}
