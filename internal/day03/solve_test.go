2078c1dfdc446d82d7131c8d2db7dfb5df556eeb
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-08-18 09:54:14 +0100

    refactor: assertion helpers

--- internal/day03/solve_test.go
+++ internal/day03/solve_test.go
@@ -8,22 +8,33 @@ func TestMultiply(t *testing.T) {
 		got, _ := Multiply(given)
 		want := 12
 
-		if got != want {
-			t.Errorf("got %d, want %d, given %s", got, want, given)
-		}
+		assertMultiplyEquals(t, got, want)
 	})
 
 	t.Run("requires two numbers", func(t *testing.T) {
 		given := "mul(,)"
 		_, err := Multiply(given)
 
-		if err == nil {
-			t.Fatal("Expected error but did not get one")
-		}
-
-		if err != ErrInsufficientMultiplyArgs {
-			t.Errorf("got %s, want %s", err, ErrInsufficientMultiplyArgs)
-		}
-
+		assertError(t, err, ErrInsufficientMultiplyArgs)
 	})
+}
+
+func assertMultiplyEquals(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("expected error but did not get one")
+	}
+
+	// compare the underlining error type
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+
 }
