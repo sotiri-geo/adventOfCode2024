df0deea6cdc15d2d2f78f7147d7959b6389aabb3
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-08-21 11:59:49 +0100

    fix: part 1

--- internal/day05/solve.go
+++ internal/day05/solve.go
@@ -2,6 +2,7 @@ package day05
 
 import (
 	"errors"
+	"fmt"
 	"slices"
 	"strconv"
 	"strings"
@@ -9,6 +10,7 @@ import (
 
 var ErrNoPageNumber = errors.New("no page number recorded as key.")
 
+// Key is a page, values is an array of predecessor pages
 type Predecessor map[int][]int
 
 // Pointer reciever (p *Predecessor)
@@ -27,4 +29,45 @@ func (p Predecessor) HasPredecessor(to, from int) (bool, error) {
 		return false, ErrNoPageNumber
 	}
 	return slices.Contains(values, from), nil
+}
+
+func (p Predecessor) IsValidOrder(pageUpdates []int) (bool, error) {
+
+	for idx, page := range pageUpdates {
+		previousPages := pageUpdates[:idx]
+		for _, previousPage := range previousPages {
+			ok, _ := p.HasPredecessor(page, previousPage)
+			if !ok {
+				errorMessage := fmt.Sprintf("Failed with page: %d, previousPage: %d", page, previousPage)
+				return false, errors.New(errorMessage)
+			}
+		}
+	}
+	return true, nil
+}
+
+func MiddleNumber(pageUpdates []int) int {
+	middleIndex := len(pageUpdates) / 2
+	return pageUpdates[middleIndex]
+}
+
+func NewPredecessor(orderingRules []string) Predecessor {
+	pre := Predecessor{}
+	for _, rule := range orderingRules {
+		pre.Add(rule)
+	}
+	return pre
+}
+
+func Part1(predecessor Predecessor, pageUpdates [][]int) int {
+	total := 0
+
+	for _, pages := range pageUpdates {
+		if ok, err := predecessor.IsValidOrder(pages); err == nil {
+			if ok {
+				total += MiddleNumber(pages)
+			}
+		}
+	}
+	return total
 }
--- internal/day05/solve_test.go
+++ internal/day05/solve_test.go
@@ -12,6 +12,9 @@ Create a map which shows for each key, which numbers must be prior to it
 
 75,47,61,53,29
 75|47, 75|61, 75|53, and 75|29.
+
+We need to see for every page, the prior pages in the update are in the currents page
+predecessors. If its not, then there exist one page thats out of order as part of update
 */
 
 func TestPredecessor(t *testing.T) {
@@ -65,6 +68,73 @@ func TestPredecessor(t *testing.T) {
 		if got != ErrNoPageNumber {
 			t.Errorf("got %q, want %q", got, want)
 		}
+	})
 
+	t.Run("Has all prior pages as predecessors", func(t *testing.T) {
+		pre := Predecessor{47: []int{70, 30}, 70: []int{30}}
+		pageUpdates := []int{30, 70, 47}
+		got, _ := pre.IsValidOrder(pageUpdates)
+		want := true
+
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Works on example input", func(t *testing.T) {
+		pre := Predecessor{47: []int{97, 75}, 61: []int{75, 47, 97}, 53: []int{47, 75, 61, 97}, 29: []int{75, 97, 53, 47, 61}}
+		pageUpdates := []int{75, 47, 61, 53, 29}
+		got, error := pre.IsValidOrder(pageUpdates)
+		want := true
+
+		t.Logf("got error: %v", error)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("init new map with defined predecessors", func(t *testing.T) {
+		orderingRules := []string{"47|53", "97|13"}
+		got := NewPredecessor(orderingRules)
+		want := Predecessor{13: []int{97}, 53: []int{47}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func TestMiddleNumber(t *testing.T) {
+	t.Run("odd length array", func(t *testing.T) {
+		array := []int{3, 4, 5}
+		got := MiddleNumber(array)
+		want := 4
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+
+	t.Run("even length array", func(t *testing.T) {
+		array := []int{7, 8, 9, 10}
+		got := MiddleNumber(array)
+		want := 9 // page number "just after" halfway point
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
+}
+
+func TestPart1(t *testing.T) {
+	t.Run("Two page updates correct", func(t *testing.T) {
+		pre := Predecessor{5: []int{1, 2, 3, 4}, 4: []int{1, 2, 3}, 3: []int{1, 2}, 2: []int{1}}
+		pageUpdates := [][]int{{1, 2, 3}, {3, 2, 1}, {1, 2, 3, 4}}
+		got := Part1(pre, pageUpdates)
+		want := 5
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
 	})
 }
